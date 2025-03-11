57bfe50408fa1cbcfe8ea7506a6f66255313b58f
Laila Khan <lailatariqkhan@gmail.com>
2025-03-11 15:35:31 +0000

    seeding data

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using ZooKeepers.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ZooDbContext>(options => options.UseSqlite(connectionString));
 
+// Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddLogging();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
